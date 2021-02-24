1ee9b85038788b37e5d0da9836510441d6371df9
Ali Yousefi <a_yousefi78@yahoo.com>
2021-02-24 21:30:17 +0330

    app completed

--- app/src/main/java/com/example/colorchange_q3/MainActivity.kt
+++ app/src/main/java/com/example/colorchange_q3/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.colorchange_q3
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import com.example.colorchange_q3.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -13,29 +16,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        val random = Random(20)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
-        var colorNum = 0
+
+        var colorNum:Int = intent.extras?.getInt("color") ?: 0
+        binding.root.setBackgroundColor(colorNum)
 
 
         binding.changeColor.setOnClickListener{
-            colorNum = random.nextInt()
+            colorNum = (Color.parseColor("#000000")..Color.parseColor("#ffffff")).random()
             binding.layout.setBackgroundColor(colorNum)
         }
 
         binding.next.setOnClickListener {
             val intent = Intent(this , MainActivity::class.java)
+            intent.putExtra("color", colorNum)
             startActivity(intent)
-            binding.layout.setBackgroundColor(colorNum)
         }
 
-//        binding.back.setOnClickListener{
-//            val callback = requireA
-//        }
+        binding.back.setOnClickListener{
+            this.onBackPressed()
+        }
 
 
 
